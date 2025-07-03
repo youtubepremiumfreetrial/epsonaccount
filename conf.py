f02c349ff8a597d50fae9335659b5f78faf40b36
youtubepremiumfreetrial <165153215+youtubepremiumfreetrial@users.noreply.github.com>
2025-07-03 14:41:45 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'HP Smart'
+project = 'Log In'
 
 
 # The full version, including alpha/beta/rc tags
